8b008e3ba89baaed9bc6e04b0e8f80e3c9392794
WatPongsapak <pongsapak.pankhao@gmail.com>
2018-11-26 11:23:09 +0700

    act can create update delete can pin but cant unpin

--- handler/handler.go
+++ handler/handler.go
@@ -4,7 +4,6 @@ import (
 	"strconv"
 	"database/sql"
 	"event-management/activity"
-	"event-management/date"
 	"event-management/pinactivity"
 	"html/template"
 	"net/http"
@@ -17,7 +16,6 @@ import (
 
 type Handler struct {
 	actManage *activity.Manager
-	dateManage *date.Manager
 	pinManage *pinactivity.Manager
 }
 
@@ -44,16 +42,48 @@ func (h *Handler)addActivityPageHandler(w http.ResponseWriter, r *http.Request) 
 	w.Header().Set("Content-Type", "text/html")
 	t,err := template.ParseFiles("html/formact.html","html/template.html")
 	err = t.ExecuteTemplate(w,"template", struct {
-		Title string
+		Mode string
 		A activity.Activity
+		Date string
+		Time string
 	}{
-		Title : "Add Activity",
+		Mode : "add",
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
+
+func (h *Handler)pinActivityPageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	t,err := template.ParseFiles("html/formpin.html","html/template.html")
+	vars := mux.Vars(r)
+	id,_ := strconv.Atoi(vars["id"])
+	fmt.Println(id)
+	a,err := h.actManage.FindByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.ExecuteTemplate(w,"template", struct {
+		A activity.Activity
+		Date string
+		Time string
+		ID int
+	}{
+		A : *a,
+		Date: a.StartDate.Format("01/02/2006")+" - "+a.EndDate.Format("01/02/2006"),
+		Time: a.StartTime.Format("03:04 pm")+" - "+a.EndTime.Format("03:04 pm"),
+		ID:id,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+
 
 func (h *Handler)editActivityPageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -68,11 +98,15 @@ func (h *Handler)editActivityPageHandler(w http.ResponseWriter, r *http.Request)
 	fmt.Println(a)
 	t,err := template.ParseFiles("html/formact.html","html/template.html")
 	err = t.ExecuteTemplate(w,"template", struct {
-		Title string
+		Mode string
 		A activity.Activity
+		Date string
+		Time string
 	}{
-		Title : "Edit Activity",
+		Mode : "edit",
 		A : *a,
+		Date: a.StartDate.Format("01/02/2006")+" - "+a.EndDate.Format("01/02/2006"),
+		Time: a.StartTime.Format("03:04 pm")+" - "+a.EndTime.Format("03:04 pm"),
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -94,8 +128,8 @@ func (h *Handler)getForm(a *activity.Activity, w http.ResponseWriter, r *http.Re
 	var max int64
 	datearr :=  strings.Split(r.FormValue("daterange"), " - ")
 	timearr :=  strings.Split(r.FormValue("timerange"), " - ")
-	starttime, _ := time.Parse("3:04 pm", timearr[0])
-	endtime, _ := time.Parse("3:04 pm", timearr[1])
+	starttime, _ := time.Parse("03:04 pm", timearr[0])
+	endtime, _ := time.Parse("03:04 pm", timearr[1])
 	startdate, _ := time.Parse("01/02/2006", datearr[0])
 	enddate, _ := time.Parse("01/02/2006", datearr[1])
 	name =  r.FormValue("name")
@@ -133,6 +167,39 @@ func (h *Handler)editActivityHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func (h *Handler)delActivityHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	vars := mux.Vars(r)
+	id,_ := strconv.Atoi(vars["id"])
+	a,err := h.actManage.FindByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = h.actManage.Delete(a)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	
+}
+
+func (h *Handler)pinActivityHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	vars := mux.Vars(r)
+	id,_ := strconv.Atoi(vars["id"])
+	name :=  r.FormValue("name")
+	employeeid :=  r.FormValue("employeeid")
+	phone :=  r.FormValue("phone")
+
+	h.pinManage.Insert(&pinactivity.Pinactivity{
+		ActivitiesID:id,
+		EmployeeCode:employeeid,
+		Name        :name,
+		Phone       :phone,
+	})
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
 
 func StartServer(addr string, db *sql.DB) error {
 
@@ -141,18 +208,19 @@ func StartServer(addr string, db *sql.DB) error {
 		actManage: &activity.Manager{
 			DB: db,
 		},
-		dateManage: &date.Manager{
-			DB: db,
-		},
 		pinManage: &pinactivity.Manager{
 			DB: db,
 		},
 	}
 	r.HandleFunc("/add_activity", h.addActivityPageHandler).Methods("GET")
 	r.HandleFunc("/edit_activity/{id}", h.editActivityPageHandler).Methods("GET")
+	r.HandleFunc("/pin_activity/{id}", h.pinActivityPageHandler).Methods("GET")
 
+	r.HandleFunc("/activity/del/{id}", h.delActivityHandler).Methods("GET")
 	r.HandleFunc("/activity/{id}", h.editActivityHandler).Methods("POST")
 	r.HandleFunc("/activity", h.addActivityHandler).Methods("POST")
+	r.HandleFunc("/pinact/{id}", h.pinActivityHandler).Methods("POST")
+	
 	r.HandleFunc("/", h.indexPageHandler).Methods("GET")
 
 	return http.ListenAndServe(addr, r)
